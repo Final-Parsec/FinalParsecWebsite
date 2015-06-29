b74931bb214672ed5517a7a24a315ca5058be09f
Baer <baer.bradford@gmail.com>
2015-06-28 21:15:46 -0500

    Script for setting up game tags.

---
+++ database/sql/v_4_0_game_tags.sql
@@ -0,0 +1,5 @@
+update tag set game_name = 'Nauticus' where name = 'nauticus';
+update tag set game_name = 'Space Snakes: Prototype' where name = 'snakes';
+update tag set game_name = 'Space Snakes: Legacy Earth' where name = 'legacy-earth';
+update tag set game_name = 'Aurora TD' where name = 'tower-defense';
+update tag set game_name = 'Unity Masters' where name = 'unity-masters';
