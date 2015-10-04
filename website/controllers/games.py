d6a6cb672eecfba3934e089f443d35be4327b2a5
Baer <baer.bradford@gmail.com>
2015-10-04 10:54:02 -0500

    Updated Aurora TD to 1.3 and added cross domain policy.

--- website/controllers/games.py
+++ website/controllers/games.py
@@ -4,6 +4,11 @@ from flask import render_template
 from website import final_parsec_website
 
 
+@final_parsec_website.route('/crossdomain.xml')
+def get_cross_domain_policy():
+    return final_parsec_website.send_static_file('crossdomain.xml')
+
+
 @final_parsec_website.route('/g/<game_tag_name>')
 def landing_page(game_tag_name):
     posts = get_posts_with_tag(game_tag_name)
